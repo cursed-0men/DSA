ee65e079aa47082ad6087303eed4d219df51b0f7
Dhyey Savaliya <136191678+cursed-0men@users.noreply.github.com>
2024-07-08 15:04:32 +0530

    Update 5_hw1.cpp

--- 2_Functions/5_hw1.cpp
+++ 2_Functions/5_hw1.cpp
@@ -2,3 +2,20 @@
 // Given AP general term => (3*n + 7)
 // input n
 // output nth term
+
+#include<iostream>
+using namespace std;
+
+int nth_term(int n){
+return (3*n)+7;
+}
+
+int main(){
+
+  int x;
+  cin>>x;
+
+  cout<<nth_term(x)<<endl;
+
+return 0;
+}
