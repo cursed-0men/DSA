dcafd82cc0d035a5fdbeede35b5128655c0f2648
Dhyey Savaliya <136191678+cursed-0men@users.noreply.github.com>
2024-07-08 15:08:19 +0530

    Update 6_hw2.cpp

--- 2_Functions/6_hw2.cpp
+++ 2_Functions/6_hw2.cpp
@@ -2,3 +2,27 @@
 
 // input a = 2, b = 3
 // output total setbits in a and b
+
+# include<iostream>
+using namespace std;
+
+int num_setbits(int n){
+int count = 0;
+
+  while(n){
+  if(n&1){
+    count++;
+  }
+n = n >> 1;
+}
+return count;
+}
+int main(){
+int x,y
+cin>>x>>y;
+
+int total_setbits = num_setbits(x) + num_setbits(y);
+cout<<total_setbits<<endl;
+
+  return 0;
+}
