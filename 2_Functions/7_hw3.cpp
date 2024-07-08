3d62eb47b74b3ad3fda7a60ea891107e4e0962e5
Dhyey Savaliya <136191678+cursed-0men@users.noreply.github.com>
2024-07-08 15:12:29 +0530

    Update 7_hw3.cpp

--- 2_Functions/7_hw3.cpp
+++ 2_Functions/7_hw3.cpp
@@ -3,3 +3,25 @@
 
 // fib : 0,1,1,2,3,5,8...
 // n = 5, 3 is answer
+
+#include<iostream>
+using namespace std;
+
+int fib(int n){
+  if(n==1){
+  return 0;
+  }
+  else if(n==2){
+  return 1;
+  }
+  else{
+    return fib(n-1)+fib(n-2);
+      }
+}
+
+int main(){
+int x;
+cin>>x;
+  cout<<fib(x)<<endl;
+return  0;
+}
